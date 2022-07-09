e18e66090d7259baee0b818aa21d131d1183b36f
mukul <mukul.kr99@gmail.com>
2022-07-09 11:57:59 +0530

    structured in classes

--- image_caption_generator.py
+++ image_caption_generator.py
@@ -2,37 +2,44 @@ from transformers import ViTFeatureExtractor, AutoTokenizer, VisionEncoderDecode
 import torch
 from PIL import Image
 
-feature_extractor = ViTFeatureExtractor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+
+class captionGenerator:
+
+  def __init__(self, max_length=16, num_beam=4) -> None:
+    self.feature_extractor = ViTFeatureExtractor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+    self.tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+    self.model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+    self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    self.model = self.model.to(self.device)
+    self.max_length = max_length
+    self.num_beams = num_beam
+    self.gen_kwargs = {"max_length": self.max_length, "num_beams": self.num_beams}
+
+  def predict_caption(self, image_paths):
+    images = []
+    for image_path in image_paths:
+      i_image = Image.open(image_path)
+      if i_image.mode != "RGB":
+        i_image = i_image.convert(mode="RGB")
+
+      images.append(i_image)
+
+    pixel_values = self.feature_extractor(images=images, return_tensors="pt").pixel_values
+    pixel_values = pixel_values.to(self.device)
+
+    output_ids = self.model.generate(pixel_values, **self.gen_kwargs)
+
+    preds = self.tokenizer.batch_decode(output_ids, skip_special_tokens=True)
+    preds = [pred.strip() for pred in preds]
+    return preds
 
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model.to(device)
+if __name__ == "__main__":
+  
+  capGen = captionGenerator()
 
-max_length = 16
-num_beams = 4
-gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
-
-def predict_step(image_paths):
-  images = []
-  for image_path in image_paths:
-    i_image = Image.open(image_path)
-    if i_image.mode != "RGB":
-      i_image = i_image.convert(mode="RGB")
-
-    images.append(i_image)
-
-  pixel_values = feature_extractor(images=images, return_tensors="pt").pixel_values
-  pixel_values = pixel_values.to(device)
-
-  output_ids = model.generate(pixel_values, **gen_kwargs)
-
-  preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
-  preds = [pred.strip() for pred in preds]
-  return preds
-
-output = predict_step(['/home/mukul/Downloads/image_4.jpeg', '/home/mukul/Downloads/image_5.jpeg',
-                       '/home/mukul/Downloads/image_6.jpeg', '/home/mukul/Downloads/image_7.jpeg'])
-                       
-print(output)
+  image_files = ['/home/mukul/Downloads/image_4.jpeg', '/home/mukul/Downloads/image_5.jpeg',
+                 '/home/mukul/Downloads/image_6.jpeg', '/home/mukul/Downloads/image_7.jpeg']
+  
+  output = capGen.predict_caption(image_files)                       
+  print(output)
--- inference.py
+++ inference.py
@@ -1,10 +1,7 @@
-from matplotlib.pyplot import draw
-from matplotlib.transforms import Transform
 import yaml
 from pathlib import Path
 from models import build_model
 from munch import DefaultMunch
-from transformers import RobertaTokenizerFast
 from PIL import Image, ImageDraw
 from  datasets.coco import make_coco_transforms
 from util.misc import NestedTensor
@@ -13,99 +10,141 @@ from util.infer_utils import infer, plot_bbox
 from models.postprocessors import PostProcess
 import torch.nn.functional as F
 
-args = {
-    'resume': '/media/mukul/Storage/Projects/MDetr/data/models/pretrained_resnet101_checkpoint.pth',
-    'model_config_file': '/media/mukul/Storage/Projects/MDetr/mdetr/configs/model_conf.yaml' 
-    }
 
-if 'model_config_file' in args:
-    with open(args['model_config_file'], 'r') as f:
-        config = yaml.safe_load(f)
-    args.update(config)
+class coRegionResolution:
 
-
-dataset = [{
-    'image_file': '/home/mukul/Downloads/WhatsApp Image 2022-06-14 at 6.01.29 PM (1).jpeg',
-    'queries': [
-        "A ant is bugging the bug.",
-        "A ant is bugging the bug.",]
-    },
-    {
-    'image_file': '/home/mukul/Downloads/WhatsApp Image 2022-06-14 at 6.01.29 PM (1).jpeg',
-    'queries': [
-        "A ant is bugging the bug.",
-        "A ant is bugging the bug.",
-    ]
-    }
-]
-
-out_dir = Path(r"/media/mukul/Storage/Projects/MDetr/data/flickr/output")
-
-transform = make_coco_transforms('val', cautious=True)
-
-args = DefaultMunch.fromDict(args)
-device = torch.device(args.device)
-
-model, criterion, _, _, _ = build_model(args)
-
-checkpoint = torch.load(args.resume, map_location="cpu")
-model.load_state_dict(checkpoint["model"])
-
-print("Model loaded")
-
-model.to(device)
-
-bbox_postprocessor = PostProcess()
-
-box_threshold = 0.9
-token_threshold = 0.1
-
-for data in dataset:
-    image_pil = Image.open(data['image_file'])
-    w, h = image_pil.size
-    image_pil = image_pil.resize((w*2, h*2))
-    w, h = image_pil.size
-    out_path = out_dir / data['image_file'].split("/")[-1]
-    drawer = ImageDraw.Draw(image_pil)
-    captions = data['queries'][:2]
-    target_size = torch.as_tensor([(int(h), int(w)), (int(h), int(w))]).to(device)
-    image, _ = transform(image_pil, {})
-    batch_images = NestedTensor.from_tensor_list([image, image], do_round=False)
-    batch_images = batch_images.to(device)
-    output = infer(model, batch_images, captions)
-    bbox_details = bbox_postprocessor(output, target_size)
-    pred_prod = F.softmax(output['pred_logits'], -1)
-    for bbox in bbox_details:
-
-        scores = bbox['scores']
-        labels = bbox['labels']
-        boxes = bbox['boxes']
-        mask = torch.ones(scores.size()[0]).to(device)
-        indices = torch.nonzero((scores >= box_threshold)*mask)
-        indices = [int(k) for k in indices.detach().cpu().numpy()]
+    def __init__(self, model_weights, model_config) -> None:      
+        self.args = {
+            'resume': model_weights,
+            'model_config_file': model_config
+            }
         
-        phrases = {}
-        for ind in indices:
-            prob = pred_prod[0][ind]
-            tok_mask = torch.ones(prob.size()[0]).to(device)
-            tok_indices = torch.nonzero((prob >=token_threshold)*tok_mask)
-            tok_indices = [int(k) for k in tok_indices.detach().cpu().numpy()]
-            beg = min(tok_indices)
-            end = max(tok_indices)
-            span_start = output['tokenized'].token_to_chars(0, beg)
-            span_end = output['tokenized'].token_to_chars(0, end)
+        with open(self.args['model_config_file'], 'r') as f:
+            self.config = yaml.safe_load(f)
+        self.args.update(self.config)
+        self.args = DefaultMunch.fromDict(self.args)
+        self.device = torch.device(self.args.device)
+        self.model, self.criterion, _, _, _ = build_model(self.args)
+        self.transform = make_coco_transforms('val', cautious=True)
+        self.checkpoint = torch.load(self.args.resume, map_location="cpu")
+        self.model.load_state_dict(self.checkpoint["model"])
+        self.model.to(self.device)
+        self.bbox_postprocessor = PostProcess()
+        self.box_threshold = 0.5
+        self.token_threshold = 0.3
+
+    def preprocess(self, image_paths):
+        images = []
+        target_sizes = []
+        
+        for image_file in image_paths:
+            image_pil = Image.open(image_file)
+            w, h = image_pil.size
+            image_pil = image_pil.resize((w*2, h*2))
+            w, h = image_pil.size
+            image, _ = self.transform(image_pil, {})
+            images.append(image)
+            target_sizes.append((int(h), int(w)))
+        batch_images = NestedTensor.from_tensor_list(images, do_round=False)
+        target_size = torch.as_tensor(target_sizes).to(self.device)
+
+        return batch_images, target_size
+
+    def plot_results(self, images, predictions, captions, out_dir, scale = 2):
+        assert len(images) == len(predictions)
+        for cap, img_path in zip(captions, images):
+            out_path = out_dir / f"{img_path.stem}.jpg"
+            pred = predictions[img_path.stem]
+            pil_image = Image.open(img_path)
+            w, h = pil_image.size
+            pil_image = pil_image.resize((w*2, h*2))
+            w, h = pil_image.size
+            drawer = ImageDraw.Draw(pil_image)
+
+            for phrase, (score, box) in pred.items():
+                drawer.rectangle(box, width=2, outline=(255, 0, 0))
+                drawer.text(box[:2], f"{phrase}-{score:2f}")
+                    
+            drawer.text([10, 10], f"{cap}")
+            pil_image.save(out_path)
+    
+    def predict(self, image_files, captions):
+
+        assert len(image_files) == len(captions), f"length of images -{len(image_files)} and captions - {captions} are different."
+        
+        batch_images, target_size = self.preprocess(image_files) 
+
+        batch_images = batch_images.to(self.device)
+        target_size = target_size.to(self.device)
+
+        output = infer(self.model, batch_images, captions)
+        bbox_details = self.bbox_postprocessor(output, target_size)
+        pred_prod = F.softmax(output['pred_logits'], -1)
+        
+        results = {}
+        for i, bbox in enumerate(bbox_details):
+            scores = bbox['scores']
+            boxes = bbox['boxes']
+            # labels = bbox['labels']
+            mask = torch.ones(scores.size()[0]).to(self.device)
+            indices = torch.nonzero((scores >= self.box_threshold)*mask)
+            indices = [int(k) for k in indices.detach().cpu().numpy()]
             
-            phrase = captions[0][span_start.start: span_end.end]
-            if phrase in phrases:
-                phrases[phrase].append((ind, float(scores[ind])))
-            else:
-                phrases[phrase] = [(ind, float(scores[ind]))]
+            phrases = {}
+            for ind in indices:
+                x1,y1,x2,y2 = boxes[ind]
+                bbox_coord = int(x1), int(y1), int(x2), int(y2)
+                prob = pred_prod[i][ind]
+                tok_mask = torch.ones(prob.size()[0]).to(self.device)
+                tok_indices = torch.nonzero((prob >=self.token_threshold)*tok_mask)
+                tok_indices = [int(k) for k in tok_indices.detach().cpu().numpy()]
+                if 255 in tok_indices:
+                    tok_indices.remove(255)
+
+                if not tok_indices:
+                    continue
+
+                beg = min(tok_indices)
+                end = max(tok_indices)
+                span_start = output['tokenized'].token_to_chars(i, beg)
+                span_end = output['tokenized'].token_to_chars(i, end)
+                
+                phrase = captions[i][span_start.start: span_end.end]
+                if phrase in phrases:
+                    current_score = phrases[phrase][0]
+                    if float(scores[ind]) > current_score:
+                        phrases[phrase] = (float(scores[ind]), bbox_coord)
+                else:
+                    phrases[phrase] = (float(scores[ind]), bbox_coord)
+
+            results[image_files[i].stem] = phrases
+
+        return results
 
 
-        plot_bbox(drawer, boxes, scores, phrases, captions[0])
-        image_pil.save(out_path)
+if __name__ == "__main__":
+    
+    model_weights = Path(r'/media/mukul/Storage/Projects/MDetr/data/models/pretrained_resnet101_checkpoint.pth')
+    model_config = Path(r'/media/mukul/Storage/Projects/MDetr/mdetr/configs/model_conf.yaml')
+    output_path = Path(r"/media/mukul/Storage/Projects/MDetr/data/flickr/output")
 
-        break
+    crr = coRegionResolution(model_weights, model_config)
 
-# dataset
-# tokenizer = RobertaTokenizerFast.from_pretrained(args.text_encoder_type, local_files_only=True)
+    images = [Path(r'/home/mukul/Downloads/image_5.jpeg')]
+
+    captions = ['A Ant and a Bug on leaf.']
+
+    out = crr.predict(images, captions)
+    crr.plot_results(images, out, captions, output_path)
+
+
+
+# Path(r'/home/mukul/Downloads/image_5.jpeg')
+# Path(r'/home/mukul/Downloads/image_4.jpeg')
+# Path(r'/home/mukul/Downloads/image_2.jpeg')
+# Path(r'/home/mukul/Downloads/image_8.jpeg')
+
+# 'A Ant and a Bug on leaf.'
+# 'A child holding a banana'
+# 'A basket filled with fruits.'
+# 'A coin and an ant laying on a surface.'
